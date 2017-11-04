3ca0fbad9266545d2ca54d0123b3f17dc5a8314f
najmul-amin <najmulamin123@gmail.com>
2017-11-05 03:34:56 +0600

    downloader

---
+++ downloader.py
@@ -0,0 +1,22 @@
+from pytube import YouTube # pip install pytube 
+print("Youtube Video Downloader")  
+inp = input("Enter Video Id : ") # example : 9bZkp7q19f0 , EHkozMIXZ8w , kJQP7kiw5Fk
+print("Collecting Data...")
+final = "https://www.youtube.com/watch?v="+inp
+yt = YouTube(final)
+title = yt.title
+print("Video Found !")    
+print("Video Title : ",title)   
+choice = input("Downlod Video or Audio 1/2 : ")
+if(choice=='1'):
+    print("Video is Downloading...") 
+    dwn = yt.streams.first()
+    dwn.download()
+    print("Downlod Complete !") 
+elif(choice=='2'):
+    print("Video is Downloading...") 
+    dwn = yt.streams.filter(only_audio=True).first()
+    dwn.download()
+    print("Downlod Complete !")   
+else:    
+    print("Sorry Wrong Input !")
